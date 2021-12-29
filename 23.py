f2211487f5fda9378b82f610f4b4a55e9c1158f0
wazawoo <reed.moseng@gmail.com>
2021-12-29 16:43:03 -0600

    Add placeholder files for remaining days

---
+++ 23.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+test_raw = """"""
+
+test = list(test_raw.splitlines())
+real = list(open(".txt").read().splitlines())
+
+# 1
+# print(f1(test))
+# print(f1(real))
+
+# 2
+# print(f2(test))
+# print(f2(real))
